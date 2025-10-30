ac634c9e0dbdce83217c1a0590c07099840e9b1d
Azkalyasin <yasinadzkiaazkal@gmail.com>
2025-10-30 15:43:42 +0700

    menmbhkan driver agar dapat memebrikan garis dengan wana dark gray

--- app/src/main/java/com/example/praktikum5/ui.kt
+++ app/src/main/java/com/example/praktikum5/ui.kt
@@ -4,9 +4,12 @@ package com.example.praktikum5
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
@@ -18,7 +21,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import org.xmlpull.v1.sax2.Driver
 
 
 @Composable
@@ -73,6 +80,15 @@ fun FormDataDiri(modifier: Modifier
                 textAlamat = it
             }
 
+        )
+        Divider(
+            modifier = Modifier.padding(
+                bottom = dimensionResource(R.dimen.pading_medium), top = dimensionResource(
+                    R.dimen.pading_medium
+                )
+            ),
+            thickness = dimensionResource(R.dimen.divider_tipis),
+            color = Color.DarkGray
         )
     }
 }
