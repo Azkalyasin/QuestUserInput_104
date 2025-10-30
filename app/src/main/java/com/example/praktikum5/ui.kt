068546828116f27acfa76cfea5a62b2e8a7dbe4a
Azkalyasin <yasinadzkiaazkal@gmail.com>
2025-10-30 15:17:48 +0700

    pembuatan variable textnama, text alamt dan textjk untuk menymoan data masukan

--- app/src/main/java/com/example/praktikum5/ui.kt
+++ app/src/main/java/com/example/praktikum5/ui.kt
@@ -1,11 +1,25 @@
 package com.example.praktikum5
 
+import android.R
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 
 @Composable
 fun FormDataDiri(modifier: Modifier
 ) {
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
+
+    var nama by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var jenis by remember { mutableStateOf("") }
+
 
 }
