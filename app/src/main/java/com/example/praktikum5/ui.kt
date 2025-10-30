69e81371b22a1f9281d4f2455e20a9cf7084f7d2
Azkalyasin <yasinadzkiaazkal@gmail.com>
2025-10-30 15:26:11 +0700

    menmbhkan textfield atu tempat masukan data khususnya data nama

--- app/src/main/java/com/example/praktikum5/ui.kt
+++ app/src/main/java/com/example/praktikum5/ui.kt
@@ -4,6 +4,10 @@ package com.example.praktikum5
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -11,6 +15,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 
 @Composable
@@ -30,6 +35,15 @@ fun FormDataDiri(modifier: Modifier
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ){
-
+        OutlinedTextField(
+            value = textNama,
+            singleLine = true,
+            shape = MaterialTheme.shapes.large,
+            modifier = Modifier.width(width = 250.dp),
+            label = { Text(text = "Nama Lengkap") },
+            onValueChange = {
+                textNama = it
+            }
+        )
     }
 }
